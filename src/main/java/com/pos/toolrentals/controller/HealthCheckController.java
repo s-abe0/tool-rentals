dbea3041bed1c6221faf9396bc48d25741f524a7
Shane Abe <shane.abe0@gmail.com>
2025-08-08 16:11:41 -0400

    src update

--- src/main/java/com/pos/toolrentals/controller/HealthCheckController.java
+++ src/main/java/com/pos/toolrentals/controller/HealthCheckController.java
@@ -9,6 +9,7 @@ public class HealthCheckController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
+        System.out.println("hi");
         return ResponseEntity.ok("OK");
     }
 }
